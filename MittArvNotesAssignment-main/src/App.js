d3d288c9d727eccc6f8fa98372e5b32192c670ab
Bucky Roberts <bollamBrothers955@gmail.com>
2024-04-12 07:39:13 +0530

    first commit

---
+++ MittArvNotesAssignment-main/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import HomePage from './pages/HomePage';
+import NoteDetail from './pages/NoteDetail';
+
+import './App.css';
+
+const App = () => {
+  return (
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" exact component={<HomePage/>} />
+        <Route path="/note/:id" component={<NoteDetail/>} />
+      </Routes>
+    </BrowserRouter>
+    
+   
+  );
+}
+
+export default App;
+
+
